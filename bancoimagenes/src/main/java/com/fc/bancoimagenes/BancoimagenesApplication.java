6f57f99c369de02f47bfeeb969aa034e415e8e2d
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-20 18:40:06 -0600

    new file:   bancoimagenes/.gitattributes 	new file:   bancoimagenes/.gitignore 	new file:   bancoimagenes/mvnw 	new file:   bancoimagenes/src/main/java/com/fc/bancoimagenes/BancoimagenesApplication.java 	new file:   bancoimagenes/src/main/resources/application.properties 	new file:   bancoimagenes/src/test/java/com/fc/bancoimagenes/BancoimagenesApplicationTests.java 	modified:   bancoimagenes/.gitignore 	bancoimagenes/.mvn/ 	bancoimagenes/mvnw.cmd 	bancoimagenes/pom.xml

---
+++ bancoimagenes/src/main/java/com/fc/bancoimagenes/BancoimagenesApplication.java
@@ -0,0 +1,13 @@
+package com.fc.bancoimagenes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoimagenesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoimagenesApplication.class, args);
+	}
+
+}
---
+++ bancoimagenes/src/test/java/com/fc/bancoimagenes/BancoimagenesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.bancoimagenes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoimagenesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
