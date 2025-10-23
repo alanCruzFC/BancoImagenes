f27b40dd9c5c3f877a593bb7fbc93e7b675d7bc1
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-23 13:23:38 -0600

    modified:   bancoimagenes/frontend/package-lock.json 	modified:   bancoimagenes/frontend/package.json 	modified:   bancoimagenes/frontend/src/app/app.routes.server.ts

--- bancoimagenes/frontend/src/app/app.routes.server.ts
+++ bancoimagenes/frontend/src/app/app.routes.server.ts
@@ -1,32 +1,8 @@
-import { LoginComponent } from './auth/login/login';
-import { UploadFormComponent } from './upload/upload-form/upload-form';
-import { ListadoComponent } from './galeria/listado/listado';
-import { UsuariosComponent } from './admin/usuarios/usuarios';
-import { AuthGuard } from './core/auth-guard';
-import { RoleGuard } from './core/role-guard';
+import { RenderMode, ServerRoute } from '@angular/ssr';
 
-
-
-export const routes = [
-  { path: 'login', component: LoginComponent },
+export const serverRoutes: ServerRoute[] = [
   {
-    path: 'upload',
-    component: UploadFormComponent,
-    canActivate: [AuthGuard, RoleGuard],
-    data: { roles: ['USUARIO'] }
-  },
-  {
-    path: 'galeria',
-    component: ListadoComponent
-  },
-  {
-    path: 'admin',
-    component: UsuariosComponent,
-    canActivate: [AuthGuard, RoleGuard],
-    data: { roles: ['ADMIN'] }
-  },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: '**', redirectTo: 'login' }
+    path: '**',
+    renderMode: RenderMode.Prerender
+  }
 ];
-
-
