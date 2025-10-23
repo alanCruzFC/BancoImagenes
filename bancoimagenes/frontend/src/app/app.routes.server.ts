4f586b2642f687a490781de08abd0516c1087980
alanCruzFC <acruzdelarosa.e@fincomun.com.mx>
2025-10-23 12:19:23 -0600

    Estructura General Front

--- bancoimagenes/frontend/src/app/app.routes.server.ts
+++ bancoimagenes/frontend/src/app/app.routes.server.ts
@@ -1,8 +1,32 @@
-import { RenderMode, ServerRoute } from '@angular/ssr';
+import { LoginComponent } from './auth/login/login';
+import { UploadFormComponent } from './upload/upload-form/upload-form';
+import { ListadoComponent } from './galeria/listado/listado';
+import { UsuariosComponent } from './admin/usuarios/usuarios';
+import { AuthGuard } from './core/auth-guard';
+import { RoleGuard } from './core/role-guard';
 
-export const serverRoutes: ServerRoute[] = [
+
+
+export const routes = [
+  { path: 'login', component: LoginComponent },
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'upload',
+    component: UploadFormComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['USUARIO'] }
+  },
+  {
+    path: 'galeria',
+    component: ListadoComponent
+  },
+  {
+    path: 'admin',
+    component: UsuariosComponent,
+    canActivate: [AuthGuard, RoleGuard],
+    data: { roles: ['ADMIN'] }
+  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
+
+
